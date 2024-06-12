b676a2c1a8ee37e9879a9dbd048ed0db12148ec9
NuReach <nureach46@gmail.com>
2024-06-12 23:05:10 +0700

    draft layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Draft from "@/components/Draft";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="relative text-white ">
+      <div className="absolute z-10 w-full mt-6 p-3 flex justify-center items-center ">
+        <Draft />
+      </div>
       <Image
           src="/cover.jpg" // Replace with your image path
           alt="Background Image" // Provide an alt text description
@@ -10,7 +14,7 @@ export default function Home() {
           height={3500}
           priority // Prioritize loading for critical backgrounds
           objectPosition="center"
-          className="dark-background z-0 h-screen  w-full  object-cover "
+          className="dark-background z-0 h-[1000px] w-full  object-cover "
           />
     </div>
   );
---
+++ src/components/Draft.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+export default function Draft() {
+  return (
+    <div className='columns-1 space-y-6 w-full'>
+        <div className='p-3 md:p-6 bg-white bg-opacity-15 rounded-lg overflow-hidden'>
+            <h1 className='font-bold text-xl'>Enemy One</h1>
+            <div className='mt-3'>
+                <h2 className='font-medium'>Pick Hero</h2>
+                <div className='mt-3 columns-3 sm:columns-5 space-y-3'>
+                    {
+                        Array(5).fill(null).map((_, i) =>(
+                            <div key={i} className='w-14 h-14 bg-white rounded-lg overflow-hidden hover:cursor-pointer'></div>
+                        ))
+                    }
+                </div>
+            </div>
+            <div className='mt-3'>
+                <h2 className='font-medium'>Ban Hero</h2>
+                <div className='mt-3 columns-3 sm:columns-5 space-y-3'>
+                    {
+                        Array(5).fill(null).map((_, i) =>(
+                            <div key={i} className='w-14 h-14 bg-white rounded-lg overflow-hidden hover:cursor-pointer'></div>
+                        ))
+                    }
+                </div>
+            </div>
+            <div className='mt-3'>
+                <h2 className='font-medium'>Expectaion</h2>
+                <div className='mt-3 columns-3 sm:columns-5 space-y-3'>
+                    {
+                        Array(10).fill(null).map((_, i) =>(
+                            <div key={i} className='w-14 h-14 bg-white rounded-lg overflow-hidden hover:cursor-pointer'></div>
+                        ))
+                    }
+                </div>
+            </div>
+            <div className='mt-3'>
+                <h2 className='font-medium'>Counter</h2>
+                <div className='mt-3 columns-3 sm:columns-5 space-y-3'>
+                    {
+                        Array(10).fill(null).map((_, i) =>(
+                            <div key={i} className='w-14 h-14 bg-white rounded-lg overflow-hidden hover:cursor-pointer'></div>
+                        ))
+                    }
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
