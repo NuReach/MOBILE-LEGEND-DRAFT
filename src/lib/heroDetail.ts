3d101bf4b7e9040fb9fd8a1598bf112697258834
NuReach <nureach46@gmail.com>
2024-06-13 18:47:35 +0700

    update hero detail list

--- src/lib/heroDetail.ts
+++ src/lib/heroDetail.ts
@@ -25,5 +25,149 @@ export const HeroDetail = [
         compatability :[
             "62","110","102"
         ]
-    }
+    },
+    {
+        heroid : "111",
+        counter : [
+            "46","73"
+        ],
+        compatability :[
+            "48","62"
+        ]
+    },
+    {
+        heroid : "99",
+        counter : [
+            "20"
+        ],
+        compatability :[
+            "50","34"
+        ]
+    },
+    {
+        heroid : "88",
+        counter : [
+            "89"
+        ],
+        compatability :[
+            "120","80","104"
+        ]
+    },
+    {
+        heroid : "87",
+        counter : [
+            "72","74"
+        ],
+        compatability :[
+            "50","115"
+        ]
+    },
+    {
+        heroid : "82",
+        counter : [
+            "88","45"
+        ],
+        compatability :[
+            "112","14"
+        ]
+    },
+    {
+        heroid : "81",
+        counter : [
+            "102","48"
+        ],
+        compatability :[
+            "44"
+        ]
+    },
+    {
+        heroid : "59",
+        counter : [
+            "123"
+        ],
+        compatability :[
+            "107","48"
+        ]
+    },
+    {
+        heroid : "49",
+        counter : [
+            "23"
+        ],
+        compatability :[
+            "82"
+        ]
+    },
+    {
+        heroid : "49",
+        counter : [
+            "82","37"
+        ],
+        compatability :[
+            "31","99"
+        ]
+    },
+    {
+        heroid : "49",
+        counter : [
+            "82","37"
+        ],
+        compatability :[
+            "31","99"
+        ]
+    },
+    {
+        heroid : "19",
+        counter : [
+            "103","116"
+        ],
+        compatability :[
+            "114","79"
+        ]
+    },
+    {
+        heroid : "9",
+        counter : [
+            "46","117"
+        ],
+        compatability :[
+            "27","55"
+        ]
+    },
+    {
+        heroid : "6",
+        counter : [
+            "34"
+        ],
+        compatability :[
+            "28","46"
+        ]
+    },
+    {
+        heroid : "4",
+        counter : [
+            "61","91"
+        ],
+        compatability :[
+            "55","85"
+        ]
+    },
+    {
+        heroid : "117",
+        counter : [
+            "98"
+        ],
+        compatability :[
+            "118","34"
+        ]
+    },
+    {
+        heroid : "92",
+        counter : [
+            "124","99"
+        ],
+        compatability :[
+            "91","36"
+        ]
+    },
 ]
